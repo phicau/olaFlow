4c465f8b99c569395f1c0f26224ea5cbfd1dc4d5
Pablo Higuera <phicau@gmail.com>
2017-06-04 16:58:06 +0800

    Added headers to comply with GNU GPL

--- solvers/olaFoamOF16ext/olaDyMFoam/olaDyMFoam.C
+++ solvers/olaFoamOF16ext/olaDyMFoam/olaDyMFoam.C
@@ -1,20 +1,26 @@
+
 /*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright held by original author
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
 License
-    This file is part of olaFoam Project.
-
-    olaFoam is free software: you can redistribute it and/or modify it
-    under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    olaFoam is distributed in the hope that it will be useful, but WITHOUT
+    This file is a derivative work of OpenFOAM.
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
     ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
     FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
     for more details.
-
     You should have received a copy of the GNU General Public License
-    along with olaFoam.  If not, see <http://www.gnu.org/licenses/>.
-
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
 Application
     olaDyMFoam
 
