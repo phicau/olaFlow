873e20614df2efa12ceabd9e2d15584e3a8d0015
Pablo Higuera <phicau@gmail.com>
2017-06-04 16:58:06 +0800

    Added headers to comply with GNU GPL

--- solvers/olaFoamOF171/olaDyMFoam/olaDyMFoam.C
+++ solvers/olaFoamOF171/olaDyMFoam/olaDyMFoam.C
@@ -1,20 +1,27 @@
 /*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011-2017 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
 License
-    This file is part of olaFoam Project.
+    This file is a derivative work of OpenFOAM.
 
-    olaFoam is free software: you can redistribute it and/or modify it
+    OpenFOAM is free software: you can redistribute it and/or modify it
     under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    olaFoam is distributed in the hope that it will be useful, but WITHOUT
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
     ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
     FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
     for more details.
 
     You should have received a copy of the GNU General Public License
-    along with olaFoam.  If not, see <http://www.gnu.org/licenses/>.
-
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
 Application
     olaDyMFoam
 
