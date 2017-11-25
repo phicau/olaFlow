f46617a056989df2200aa0225941436ca40a63c4
Pablo Higuera <phicau@gmail.com>
2017-11-25 16:13:36 +0800

    Compatibility with FOAM-extend 4.0

---
+++ solvers/olaFoamFE40/olaDyMFoam/olaDyMFoam.C
@@ -0,0 +1,182 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | foam-extend: Open Source CFD
+   \\    /   O peration     | Version:     4.0
+    \\  /    A nd           | Web:         http://www.foam-extend.org
+     \\/     M anipulation  | For copyright notice see file Copyright
+-------------------------------------------------------------------------------
+License
+    This file is a derivative work of foam-extend.
+
+    foam-extend is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    foam-extend is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with foam-extend.  If not, see <http://www.gnu.org/licenses/>.
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+Application
+    olaDyMFoam
+
+Description
+    Solver for 2 incompressible, isothermal immiscible fluids using a VOF
+    (volume of fluid) phase-fraction based interface capturing approach,
+    with optional mesh motion and mesh topology changes including adaptive
+    re-meshing.
+
+    OLADYMFOAM solves the VARANS equations.
+
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+| olaFoam Project                                       ll                    |
+|                                                       l l                   |
+|   Coder: Pablo Higuera Caubilla                 ooo   l l     aa            |
+|   Bug reports: phicau@gmail.com                o   o  l l    a  a           |
+|                                                o   o  ll   l a  aa  aa      |
+|                                                 ooo    llll   aa  aa        |
+|                                                                             |
+|                                                FFFFF OOOOO AAAAA M   M      |
+|                                                F     O   O A   A MM MM      |
+|  Formerly IHFOAM Project                       FFFF  O   O AAAAA M M M      |
+|  Work initially developed at IH Cantabria      F     O   O A   A M   M      |
+|                                                F     OOOOO A   A M   M      |
+|   -----------------------------------------------------------------------   |
+| References:                                                                 |
+|                                                                             |
+| - Three-dimensional interaction of waves and porous coastal structures      |
+|    using OpenFOAM. Part I: Formulation and validation.                      |
+|    Higuera, P., Lara, J.L. and Losada, I.J. (2014)                          |
+|    Coastal Engineering, Vol. 83, 243-258.                                   |
+|    http://dx.doi.org/10.1016/j.coastaleng.2013.08.010                       |
+|                                                                             |
+| - Three-dimensional interaction of waves and porous coastal structures      |
+|    using OpenFOAM. Part II: Application.                                    |
+|    Higuera, P., Lara, J.L. and Losada, I.J. (2014)                          |
+|    Coastal Engineering, Vol. 83, 259–270.                                   |
+|    http://dx.doi.org/10.1016/j.coastaleng.2013.09.002                       |
+|                                                                             |
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "dynamicFvMesh.H"
+#include "MULES.H"
+#include "subCycle.H"
+#include "interfaceProperties.H"
+#include "twoPhaseMixture.H"
+#include "turbulenceModel.H"
+#include "pimpleControl.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+#   include "setRootCase.H"
+#   include "createTime.H"
+#   include "createDynamicFvMesh.H"
+
+    pimpleControl pimple(mesh);
+
+#   include "readGravitationalAcceleration.H"
+#   include "initContinuityErrs.H"
+#   include "createFields.H"
+#   include "createControls.H"
+#   include "correctPhi.H"
+#   include "CourantNo.H"
+#   include "setInitialDeltaT.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info<< "\nStarting time loop\n" << endl;
+
+    while (runTime.run())
+    {
+#       include "readControls.H"
+#       include "CourantNo.H"
+
+        // Make the fluxes absolute
+        fvc::makeAbsolute(phi, U);
+
+#       include "setDeltaT.H"
+
+        runTime++;
+
+        Info<< "Time = " << runTime.timeName() << nl << endl;
+
+        bool meshChanged = mesh.update();
+        reduce(meshChanged, orOp<bool>());
+
+#       include "volContinuity.H"
+
+        volScalarField gh("gh", g & mesh.C());
+        surfaceScalarField ghf("ghf", g & mesh.Cf());
+
+        if (correctPhi && meshChanged)
+        {
+#           include "correctPhi.H"
+        }
+
+        // Make the fluxes relative to the mesh motion
+        fvc::makeRelative(phi, U);
+
+        if (checkMeshCourantNo)
+        {
+#           include "meshCourantNo.H"
+        }
+
+        // Pressure-velocity corrector
+        while (pimple.loop())
+        {
+            twoPhaseProperties.correct();
+
+#           include "alphaEqnSubCycle.H"
+
+#           include "UEqn.H"
+
+            // --- PISO loop
+            while (pimple.correct())
+            {
+                    #           include "pEqn.H"
+            }
+
+            p = pd + rho*gh;
+
+            if (pd.needReference())
+            {
+                p += dimensionedScalar
+                (
+                    "p",
+                    p.dimensions(),
+                    pRefValue - getRefCellValue(p, pdRefCell)
+                );
+            }
+
+            turbulence->correct();
+        }
+
+        runTime.write();
+        // Write Porous Variables
+        if( activePorosity && runTime.outputTime() ) 
+        {
+            porosity.write();
+            porosityIndex.write();
+        }
+
+        Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+            << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+            << nl << endl;
+    }
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
---
+++ solvers/olaFoamFE40/olaFoam.C
@@ -0,0 +1,163 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | foam-extend: Open Source CFD
+   \\    /   O peration     | Version:     4.0
+    \\  /    A nd           | Web:         http://www.foam-extend.org
+     \\/     M anipulation  | For copyright notice see file Copyright
+-------------------------------------------------------------------------------
+License
+    This file is a derivative work of foam-extend.
+
+    foam-extend is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    foam-extend is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with foam-extend.  If not, see <http://www.gnu.org/licenses/>.
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+Application
+    olaFoam
+
+Description
+    Solver for 2 incompressible, isothermal immiscible fluids using a VOF
+    (volume of fluid) phase-fraction based interface capturing approach.
+
+    The momentum and other fluid properties are of the "mixture" and a single
+    momentum equation is solved.
+
+    Turbulence modelling is generic, i.e.  laminar, RAS or LES may be selected.
+
+    For a two-fluid approach see twoPhaseEulerFoam.
+
+    OLAFOAM solves the VARANS equations.
+
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+| olaFoam Project                                       ll                    |
+|                                                       l l                   |
+|   Coder: Pablo Higuera Caubilla                 ooo   l l     aa            |
+|   Bug reports: phicau@gmail.com                o   o  l l    a  a           |
+|                                                o   o  ll   l a  aa  aa      |
+|                                                 ooo    llll   aa  aa        |
+|                                                                             |
+|                                                FFFFF OOOOO AAAAA M   M      |
+|                                                F     O   O A   A MM MM      |
+|  Formerly IHFOAM Project                       FFFF  O   O AAAAA M M M      |
+|  Work initially developed at IH Cantabria      F     O   O A   A M   M      |
+|                                                F     OOOOO A   A M   M      |
+|   -----------------------------------------------------------------------   |
+| References:                                                                 |
+|                                                                             |
+| - Three-dimensional interaction of waves and porous coastal structures      |
+|    using OpenFOAM. Part I: Formulation and validation.                      |
+|    Higuera, P., Lara, J.L. and Losada, I.J. (2014)                          |
+|    Coastal Engineering, Vol. 83, 243-258.                                   |
+|    http://dx.doi.org/10.1016/j.coastaleng.2013.08.010                       |
+|                                                                             |
+| - Three-dimensional interaction of waves and porous coastal structures      |
+|    using OpenFOAM. Part II: Application.                                    |
+|    Higuera, P., Lara, J.L. and Losada, I.J. (2014)                          |
+|    Coastal Engineering, Vol. 83, 259–270.                                   |
+|    http://dx.doi.org/10.1016/j.coastaleng.2013.09.002                       |
+|                                                                             |
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "MULES.H"
+#include "subCycle.H"
+#include "interfaceProperties.H"
+#include "twoPhaseMixture.H"
+#include "turbulenceModel.H"
+#include "pimpleControl.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+#   include "setRootCase.H"
+#   include "createTime.H"
+#   include "createMesh.H"
+
+    pimpleControl pimple(mesh);
+
+#   include "readGravitationalAcceleration.H"
+#   include "initContinuityErrs.H"
+#   include "createFields.H"
+#   include "createTimeControls.H"
+#   include "correctPhi.H"
+#   include "CourantNo.H"
+#   include "setInitialDeltaT.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info<< "\nStarting time loop\n" << endl;
+
+    while (runTime.run())
+    {
+#       include "readTimeControls.H"
+#       include "CourantNo.H"
+#       include "setDeltaT.H"
+
+        runTime++;
+
+        Info<< "Time = " << runTime.timeName() << nl << endl;
+
+        // Pressure-velocity corrector
+        while (pimple.loop())
+        {
+            twoPhaseProperties.correct();
+
+#           include "alphaEqnSubCycle.H"
+
+#           include "UEqn.H"
+
+            // --- PISO loop
+            while (pimple.correct())
+            {
+#               include "pEqn.H"
+            }
+
+#           include "continuityErrs.H"
+
+            p = pd + rho*gh;
+
+            if (pd.needReference())
+            {
+                p += dimensionedScalar
+                (
+                    "p",
+                    p.dimensions(),
+                    pRefValue - getRefCellValue(p, pdRefCell)
+                );
+            }
+
+            turbulence->correct();
+        }
+
+        runTime.write();
+        // Write Porous Variables
+        if( activePorosity && runTime.outputTime() ) 
+        {
+            porosity.write();
+            porosityIndex.write();
+        }
+
+        Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+            << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+            << nl << endl;
+    }
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
